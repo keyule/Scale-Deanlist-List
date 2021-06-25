ffec7b04860e99ff03b343da1831d93e066af2a2
keyule <keyule@gmail.com>
2021-06-25 22:57:58 +0800

    some stuff

--- scraper.py
+++ scraper.py
@@ -6,19 +6,19 @@ import time
 baseurl = 'https://api.accredible.com/v1/credential/generate_certificate_pdf?mode=pdf&credential_id='
 dirname = os.path.dirname(__file__)
 
-for x in range(500):
-    id = 34255600 + x 
+for x in range(1000):
+    id = 28012550 + x 
     url = baseurl + str(id)
 
     try:
         r = requests.get(url, allow_redirects=True, timeout=6)
         filename = str(id) + '.pdf'
-        completeName = os.path.join (dirname,'PDFS','20202021Sem2',filename)
-        if(len(r.content) > 50000):
+        completeName = os.path.join (dirname,'PDFS','test',filename)
+        if(len(r.content) > 100000 and len(r.content) < 150000):
             print(f"{id} downloaded")
             open(completeName, 'wb').write(r.content)
         else:
-            print(f"{id} empty")
+            print(f"{id} wrongsize")
     except:
             print(f"{id} timeout")
 
